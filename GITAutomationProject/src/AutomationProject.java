5a6dcb0e83d6e184a7ceca30818e0af7b1eca6d0
vengateshgithub <venkateshduraisamy@live.com>
2019-06-16 10:28:25 +0530

    all work has done
    Signed-off-by: vengateshgithub <venkateshduraisamy@live.com>

---
+++ GITAutomationProject/src/AutomationProject.java
@@ -0,0 +1,11 @@
+
+public class AutomationProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Automation Process with GIT");
+
+	}
+
+}
